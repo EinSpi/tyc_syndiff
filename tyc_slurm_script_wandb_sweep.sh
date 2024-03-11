e84b17bfe8545663618b3d8f637743d90f174345
EinSpi <108343942+EinSpi@users.noreply.github.com>
2024-03-11 21:35:10 +0100

    Add files via upload

---
+++ tyc_slurm_script_wandb_sweep.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yichentang777@gmail.com
+#SBATCH --job-name=TYC_sweep
+#SBATCH --output=TYC_sweep-%A.out 
+#SBATCH --error=TYC_sweep-%A.err
+#SBATCH --time=0-12:00:00 
+#SBATCH --gres=gpu:1  # Number of GPUs if needed#
+#SBATCH --mem=24G # Memory in GB 
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+
+
+ml miniconda3
+source activate syndiff
+cd /home/guests/yichen_tang/syndiff
+export CC=gcc
+export CXX=g++
+python wandbsweep.py
+ml -miniconda3 # unload all modules
